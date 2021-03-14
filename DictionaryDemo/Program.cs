9a430736a9190174b3f455c18bd1b83ea228228a
Çetin Ürün <cetinurun.12@outlook.com>
2021-03-14 22:12:38 +0300

    14 March Class

--- DictionaryDemo/Program.cs
+++ DictionaryDemo/Program.cs
@@ -6,14 +6,14 @@ namespace DictionaryDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n  ~~~~ Welcome to our bakery ~~~~\n");
+            Console.WriteLine("\n  ~~~~ Welcome to our shop ~~~~\n");
             MyDictionary<string, float> myDictionary = new MyDictionary<string, float>();
-            Console.WriteLine("Please enter the number of desserts to add");
+            Console.WriteLine("Please enter the number of keys to add");
             int numb = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("___________________________________________________\n");
             for (int i = 1; i <= numb; i++)
             {
-                Console.WriteLine("[" + i + "]  Please enter the dessert first, then the price");
+                Console.WriteLine("[" + i + "]  Please enter the key first, then the value");
                 myDictionary.Add(Console.ReadLine(), float.Parse(Console.ReadLine()));
             }
             Console.WriteLine("\n___________________________________________________\n");
